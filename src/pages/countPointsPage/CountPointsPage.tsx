3a0f665ee9c6dc95a5a67196da9f1201080c7052
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-06-04 12:56:38 +0900

    feat: add the Result calculation

--- src/pages/countPointsPage/CountPointsPage.tsx
+++ src/pages/countPointsPage/CountPointsPage.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Radio, RadioChangeEvent, Button } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { Radio, RadioChangeEvent, Button, Typography } from 'antd'
 import { RadioGroupOptionType, RadioGroupButtonStyle } from 'antd/lib/radio'
 import { SyncOutlined } from '@ant-design/icons'
 
@@ -9,10 +9,49 @@ import styles from './CountPointsPage.module.css'
 type CountPointsFormOptionField = 'type' | 'winType' | 'headType' | 'readyType'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type CountPointsFormOption = Record<CountPointsFormOptionField, any> & { [key: string]: any }
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type CountPointsFormStates = Record<CountPointsFormOptionField, [any, React.Dispatch<any>]> & { [key: string]: any } // prettier-ignore
+type CountPointsFormStates = Record<
+  CountPointsFormOptionField,
+  [string, React.Dispatch<string>]
+> & { [key: string]: [string, React.Dispatch<string>] }
+
+const options: CountPointsFormOption = {
+  type: {
+    label: '类型',
+    value: [
+      { label: '其他', value: '其他' },
+      { label: '七对', value: '七对' },
+      { label: '平胡', value: '平胡' }
+    ]
+  },
+  winType: {
+    label: '胡牌类型',
+    value: [
+      { label: '其他', value: '其他' },
+      { label: '自摸', value: '自摸' },
+      { label: '门清荣和', value: '门清荣和' }
+    ]
+  },
+  headType: {
+    label: '雀头类型',
+    value: [
+      { label: '其他', value: '其他' },
+      { label: '役牌', value: '役牌' }
+    ]
+  },
+  readyType: {
+    label: '听牌类型',
+    value: [
+      { label: '两面/双碰', value: '两面/双碰' },
+      { label: '坎/边/单骑', value: '坎/边/单骑' }
+    ]
+  }
+}
+
+// eslint-disable-next-line comma-spacing
+const zip = <T,>(rows: Array<Array<T>>) => rows[0].map((_, c) => rows.map((row) => row[c]))
 
 export const CountPointsPage: React.FC = () => {
+  // ****************** state定义
   const states: CountPointsFormStates = {
     type: useState('其他'),
     winType: useState('其他'),
@@ -41,6 +80,46 @@ export const CountPointsPage: React.FC = () => {
     }
   })
 
+  const [result, setResult] = useState<number | string>(30)
+
+  // ****************** 符数计算
+  useEffect(() => {
+    let result: number | string = 'err'
+
+    if (states.type[0] === '七对') {
+      result = 25
+    } else if (states.type[0] === '平胡') {
+      if (states.winType[0] === '自摸') {
+        result = 20
+      } else if (states.winType[0] === '门清荣和') {
+        result = 30
+      } else {
+        result = 'error'
+      }
+    } else {
+      const winTypeScore = states.winType[0] === '其他' ? 0 : states.winType[0] === '自摸' ? 2 : 10
+      const headTypeScore = states.headType[0] === '役牌' ? 2 : 0
+      const readyTypeScore = states.readyType[0] === '坎/边/单骑' ? 2 : 0
+
+      let concealedTripletScore = 0
+      // 按幺九牌/中张牌取出
+      for (const tileTypeValue of Object.values(concealedTriplet)) {
+        // 按明/暗,,刻/杠取出
+        for (const elementsTypeValue of Object.values(tileTypeValue)) {
+          concealedTripletScore +=
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (elementsTypeValue as any).score * (elementsTypeValue as any).count
+        }
+      }
+      console.log('concealedTripletScore', concealedTripletScore)
+
+      result = 20 + winTypeScore + headTypeScore + readyTypeScore + concealedTripletScore
+      result = result === 20 ? 30 : result
+    }
+    setResult(result)
+  }, [concealedTriplet, ...Object.keys(states).map((key) => states[key][0])])
+
+  // ****************** 单选框Props creator, 主要定义onChange事件
   const RGProps: { optionType: RadioGroupOptionType; buttonStyle: RadioGroupButtonStyle } = {
     optionType: 'button',
     buttonStyle: 'solid'
@@ -58,42 +137,6 @@ export const CountPointsPage: React.FC = () => {
     }
   }
 
-  const options: CountPointsFormOption = {
-    type: {
-      label: '类型',
-      value: [
-        { label: '其他', value: '其他' },
-        { label: '七对', value: '七对' },
-        { label: '平胡', value: '平胡' }
-      ]
-    },
-    winType: {
-      label: '胡牌类型',
-      value: [
-        { label: '其他', value: '其他' },
-        { label: '自摸', value: '自摸' },
-        { label: '门清荣和', value: '门清荣和' }
-      ]
-    },
-    headType: {
-      label: '雀头类型',
-      value: [
-        { label: '其他', value: '其他' },
-        { label: '役牌', value: '役牌' }
-      ]
-    },
-    readyType: {
-      label: '听牌类型',
-      value: [
-        { label: '两面/双碰', value: '两面/双碰' },
-        { label: '坎/边/单骑', value: '坎/边/单骑' }
-      ]
-    }
-  }
-
-  // eslint-disable-next-line comma-spacing
-  const zip = <T,>(rows: Array<Array<T>>) => rows[0].map((_, c) => rows.map((row) => row[c]))
-
   return (
     <div
       style={{
@@ -103,6 +146,7 @@ export const CountPointsPage: React.FC = () => {
         margin: '0 auto'
       }}
     >
+      {/* ****************** 渲染单选框 ****************** */}
       {Object.keys(options).map((optionName) => {
         return (
           <div
@@ -122,6 +166,7 @@ export const CountPointsPage: React.FC = () => {
           </div>
         )
       })}
+      {/* ****************** 刻子杠子表格 ****************** */}
       <div style={{ marginTop: '20px', textAlign: 'left' }}>统计刻子的种类: </div>
       <table className={styles['concealed-triplet-table']}>
         <thead>
@@ -172,6 +217,17 @@ export const CountPointsPage: React.FC = () => {
           })}
         </tbody>
       </table>
+      <div
+        style={{
+          marginTop: '20px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'baseline'
+        }}
+      >
+        <Typography.Title>{Math.ceil((result as number) / 10) * 10}符</Typography.Title>
+        <span style={{ marginLeft: '5px', fontSize: 'larger' }}>({result})</span>
+      </div>
       <div
         style={{
           textAlign: 'right',
